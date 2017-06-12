c363fff262987cdf7e4dd8c6d4626b221f896b75
Tvolex <tisyanchin13@gmail.com>
2017-06-13 01:14:05 +0300

    ...

--- src/routes/randomPhoto.js
+++ src/routes/randomPhoto.js
@@ -6,7 +6,6 @@ const randomPhoto = router.get('/photo/:w/:h', (req, res) => {
     var width = req.params.w;
     var height = req.params.h;
     var uri = `https://source.unsplash.com/random/${width}x${height}`;
-
     Request(uri, (request, response, body) =>{
         var url = response.request.uri.href;
         console.log(url);
